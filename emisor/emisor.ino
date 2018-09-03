3d0ac2e8b0c81a8013e855d057c4c08764fae28a
joni182 <jcerezuelalopez@gmail.com>
2018-09-03 18:46:21 +0200

    configurado tres pulsadores

--- emisor/emisor.ino
+++ emisor/emisor.ino
@@ -6,11 +6,17 @@
 #define CE_PIN 9
 #define CSN_PIN 10
 
-const int inputPin = 4;
-int dato[1];
+//Declaremos los pines de los pulsadores
+#define PULSADOR_1 2
+#define PULSADOR_2 3
+#define PULSADOR_3 4
+
+const char ABIERTO[] = "ABIERTO";
+const char CERRADO[] = "CERRADO";
+const char TEST[] = "TEST";
 
 //Variable con la dirección del canal por donde se va a transmitir
-byte direccion[5] ={'c','a','n','a','l'};
+byte direccion[5] = {'c', 'a', 'n', 'a', 'l'};
 
 //creamos el objeto radio (NRF24L01)
 RF24 radio(CE_PIN, CSN_PIN);
@@ -19,36 +25,50 @@ RF24 radio(CE_PIN, CSN_PIN);
 
 void setup()
 {
-  pinMode(inputPin, INPUT);
-  //inicializamos el NRF24L01 
+  //Declaramos pines de entrada
+  pinMode(PULSADOR_1, INPUT);
+  pinMode(PULSADOR_2, INPUT);
+  pinMode(PULSADOR_3, INPUT);
+
+  //inicializamos el NRF24L01
   radio.begin();
   //inicializamos el puerto serie
-  Serial.begin(9600); 
- 
-//Abrimos un canal de escritura
- radio.openWritingPipe(direccion);
+  Serial.begin(9600);
 
- dato[0] = 9;
- 
+  //Abrimos un canal de escritura
+  radio.openWritingPipe(direccion);
+
+
 }
- 
+
 void loop()
 {
-  if (digitalRead(inputPin) == HIGH) {
-      bool ok = radio.write(dato, sizeof(dato));
-      //reportamos por el puerto serial los datos enviados 
-      if(ok)
-      {
-        Serial.print("Dato enviado: "); 
-        Serial.print(dato[0]); 
-     }
-     else
-     {
-       Serial.println("no se ha podido enviar");
-     }
+  while (!digitalRead(PULSADOR_1) == HIGH && !digitalRead(PULSADOR_2) == HIGH && !digitalRead(PULSADOR_3) == HIGH) {}
+
+  if (digitalRead(PULSADOR_1) == HIGH) {
+    Serial.println(ABIERTO);
+    bool ok = radio.write(ABIERTO, sizeof(ABIERTO));
+    
+    //reportamos por el puerto serial los datos enviados
+    if (ok)
+    {
+      Serial.print("Datos enviados: ");
+      Serial.println(ABIERTO);
+
+    }
+    else
+    {
+      Serial.println("no se ha podido enviar");
+    }
   }
-  else {
-      Serial.println("Apagado");
+
+  if (digitalRead(PULSADOR_2) == HIGH) {
+    Serial.println(CERRADO);
   }
-  delay(10);
+
+  if (digitalRead(PULSADOR_3) == HIGH) {
+    Serial.println(TEST);
+  }
+
+  delay(250);
 }
